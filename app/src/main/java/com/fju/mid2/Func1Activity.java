c2f027429f725c3497aed4f486f3715b2b36381a
wanyu0409 <jasperwendy0409@gmail.com>
2017-12-05 12:02:50 +0800

    Roll

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -2,12 +2,18 @@ package com.fju.mid2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Func1Activity extends AppCompatActivity {
+
+    private Button random;
+    int n = (int) (Math.random()*6+1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+        random = (Button) findViewById(R.id.random);
     }
+   
 }
