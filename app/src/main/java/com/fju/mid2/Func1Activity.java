bb8d3d5efb904ddb62c8b3d03a2f18bd83e1bb8a
wanyu0409 <jasperwendy0409@gmail.com>
2017-12-08 15:55:35 +0800

    Roll

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,19 +1,26 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class Func1Activity extends AppCompatActivity {
+import java.util.Random;
 
-    private Button random;
-    int n = (int) (Math.random()*6+1);
+public class Func1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
-        random = (Button) findViewById(R.id.random);
     }
-   
+    public void number (View view){
+        Random random = new Random();
+        int r = random.nextInt(6)+1;
+        new AlertDialog.Builder(this)
+                .setMessage(""+ r)
+                .setPositiveButton("OK", null)
+                .show();
+    }
 }
